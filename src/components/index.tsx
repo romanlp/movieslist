3de4ec8257ae416dcff385cdf5d5c8aa2e7e5f6a
romanlp <roman.lapacherie@gmail.com>
2020-09-20 23:05:56 +0100

    add hmr and build script

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,6 +2,12 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {App} from "./App";
 
+declare const module: any;
+console.log(module);
+if (module.hot) {
+    module.hot.accept();
+}
+
 ReactDOM.render(
     <App userName="Developer" lang="TypeScript"/>,
     document.getElementById("output")
